9c7124e2f9fbc00f619bdab64592d435ff10759e
Yuriy Chekan <chekanyr@icloud.com>
2024-06-01 21:44:35 +0300

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Юрий Чекан on 01.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
